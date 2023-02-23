f3a940a14469d96552c436aeb95871db7238a6c2
Dmitry Gulakov <essencegodof@gmail.com>
2023-02-23 13:16:58 +0300

    now is working  =D

--- task19.cs
+++ task19.cs
@@ -1,13 +1,14 @@
 /**/
 
-string input = Console.ReadLine("Write a 5-digit number");
+Console.WriteLine("write a 5-digit number:");
+string input = Console.ReadLine();
 
-number = int.Parse(input);
+int number = int.Parse(input);
 if ((10000 <= number) && (number <= 99999)) {
-    int n1 = int.Parse(input[0]);
-    int n2 = int.Parse(input[1]);
-    int n4 = int.Parse(input[3]);
-    int n5 = int.Parse(input[4]);
+    var n1 = input[0];
+    var n2 = input[1];
+    var n4 = input[3];
+    var n5 = input[4];
 
     if ((n1 == n5) && (n2 == n4)) {
         Console.WriteLine("да");
